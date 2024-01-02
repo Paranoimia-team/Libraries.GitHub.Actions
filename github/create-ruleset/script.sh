c406e26c36064120fa95a845eb625ce395bdf41a
NickM <paranoimia.nickm@gmail.com>
2024-01-02 04:24:32 +0200

    fix: fixed bash scripts
    - moved bash scripts into separate files

---
+++ github/create-ruleset/script.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+while getopts owner:repository:token:api_version:ruleset_path: flag
+do
+    case "${flag}" in
+        owner) owner=${OPTARG};;
+        repository) repository=${OPTARG};;
+        token) token=${OPTARG};;
+        api_version) api_version=${OPTARG};;
+        ruleset_path) ruleset_path=${OPTARG};;
+    esac
+done
+
+curl \
+    -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $token" \
+    -H "X-GitHub-Api-Version: $api_version" \
+    https://api.github.com/repos/$owner/$repository/rulesets \
+    --json "@$ruleset_path"
---
+++ github/get-project-node-id/script.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+while getopts organization:project:token: flag
+do
+    case "${flag}" in
+        organization) organization=${OPTARG};;
+        project) project=${OPTARG};;
+        token) token=${OPTARG};;
+    esac
+done
+
+get_query()
+{
+cat <<EOF
+    query 
+    { 
+        organization(login: $organization) 
+        { 
+            projectV2(number: $project) 
+            { 
+                id 
+            }
+        } 
+    }
+EOF
+}
+
+get_body()
+{
+cat <<EOF
+    {
+        "query": "$(get_query)"
+    }
+EOF
+}
+
+result=$( \
+    curl \
+        -X POST \
+        -H "Authorization: Bearer $token" \
+        https://api.github.com/graphql \
+        --json $(get_body) \
+)
+
+"result=$result" >> "$GITHUB_OUTPUT"
