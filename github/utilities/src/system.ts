260ed279f4182e856eb27209a2221592b2b38a86
NickM <paranoimia.nickm@gmail.com>
2024-01-21 05:41:37 +0200

    fix: setIfNotEmpty implementation provided

--- github/utilities/src/system.ts
+++ github/utilities/src/system.ts
@@ -6,4 +6,14 @@ declare global
     }
 }
 
+Object.prototype.setIfNotEmpty = function <TThis extends object>(this: TThis, key: keyof TThis, value: TThis[typeof key]): TThis
+{
+    if (value !== undefined && value !== null)
+    {
+        this[key] = value;
+    }
+
+    return this;
+};
+
 export {}
